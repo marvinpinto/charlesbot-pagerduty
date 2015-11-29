c11a19959b2d17de2f038f79306ecb0e3630ecc6
Marvin Pinto <marvin@pinto.im>
2015-11-28 19:12:35 -0500

    Update the charlesbot-pagerduty version from 0.1.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name='charlesbot-pagerduty',
-    version='0.1.0',
+    version='1.0.0',
     description="A charlesbot pagerduty plugin",
     long_description=readme,
     author="Marvin Pinto",
